2de0358a8e3eb676704290973b73af1bafc2e032
Washington Borges <washington.rborges@gmail.com>
2023-04-30 16:57:13 -0300

    Add Angular Material theme Purple/Green + Refactoring component Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,48 @@
+//Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginModule } from '@modules/auth/login-module';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from '@components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+//Custom 
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from '@components/login/login.component';
+import { HomeComponent } from '@components/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
+    //Custom    
+    SharedModule,
+    //Angular
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    LoginModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    //Material
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule
   ],
+  exports: [SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
